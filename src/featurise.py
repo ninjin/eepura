2bf656576d029c9902ad810eb2cad2f0f7ab6645
Pontus Stenetorp <pontus@stenetorp.se>
2012-03-19 20:31:44 +0900

    A first featuriser

---
+++ src/featurise.py
@@ -0,0 +1,116 @@
+#!/usr/bin/env python
+
+'''
+Featurise for negation and speculation classification.
+
+Author:     Pontus Stenetorp    <pontus stenetorp se>
+Version:    2011-03-07
+'''
+
+from argparse import ArgumentParser, FileType
+from sys import stderr
+
+from lib.corpora import read_corpora_dir
+
+# TODO: Start very very simple, build up-wards
+# TODO: Initially share the same feature set
+
+### Constants
+ARGPARSER = ArgumentParser()#XXX:
+ARGPARSER.add_argument('negation_features', type=FileType('w'))
+ARGPARSER.add_argument('speculation_features', type=FileType('w'))
+ARGPARSER.add_argument('corpora_dir', nargs='+')
+ARGPARSER.add_argument('-v', '--verbose', action='store_true')
+#TODO: Feature set argument
+###
+
+# TODO: Embed different feature sets
+
+def _featurise_negated(_id, id_to_ann):
+    e_ann = id_to_ann[_id]
+    trigg_ann = id_to_ann[e_ann.trigger]
+    # ^ does not occur in the train and dev sets, so it is relatively safe:
+    #    sed -e 's| ||g' -e 's|\(.\)|\1\n|g' \
+    #        res/ann/{epi,id,ge}/{train,dev}/*.txt | sed '/^$/d' | sort \
+    #        | uniq -c | sort -n -r | grep '\^'
+    trigger_text = trigg_ann.comment.replace(' ', '^')
+
+    # Feature: The actual text
+    yield 'TRIGGER-TEXT-{}'.format(trigger_text)
+
+    # Feature: Prefixes
+    for size in xrange(2, min(8, len(trigger_text) + 1)):
+        yield 'TRIGGER-PREFIX-{}-{}'.format(size, trigger_text[:size])
+
+    # TODO: Feature, neighbour words
+
+    # Event type, actually boosts us a bit, we'll come back to this later
+    #yield 'EVENT-TYPE-{}'.format(e_ann.type)
+
+def _featurise_speculated(_id, id_to_ann):
+    for e in _featurise_negated(_id, id_to_ann):
+        yield e
+
+def main(args):
+    argp = ARGPARSER.parse_args(args[1:])
+
+    seen_negated = 0
+    seen_speculated = 0
+    for corpora_dir in argp.corpora_dir:
+        for doc_id, ann_it in read_corpora_dir(corpora_dir):
+            if argp.verbose:
+                print >> stderr, 'Reading document id: {}'.format(doc_id)
+
+            # Map between annotations and their ids for easy look-up
+            ann_id_to_ann = {}
+           
+            negated = set()
+            speculated = set()
+            for ann_i, ann in enumerate(a for a in ann_it
+                    if not isinstance(a, str)):
+                ann_id_to_ann[ann.id] = ann
+                if ann.type == 'Negation':
+                    negated.add(ann.target)
+                elif ann.type == 'Speculation':
+                    speculated.add(ann.target)
+        
+            # XXX: Old, remove
+            '''
+            try:
+                negated = set(ann_id_to_ann[m.target] for m in negations)
+                speculated = set(ann_id_to_ann[m.target] for m in speculations)
+            except KeyError, e:
+                print >> stderr, ('ERROR: Modifier referred to an event with id '
+                        '{} for document id {}, but no such event was found'
+                        ).format(e.message, doc_id)
+                print >> stderr, ann_id_to_ann
+                return -1
+            '''
+        
+            seen_negated += len(negated)
+            seen_speculated += len(speculated)
+
+            for e_ann_id in (a for a in ann_id_to_ann if a.startswith('E')):
+                neg_f_tup = _featurise_negated(e_ann_id, ann_id_to_ann)
+                argp.negation_features.write('{}\t{}\n'.format(
+                    1 if e_ann_id in negated else 0,
+                    ' '.join('{}:1'.format(f)
+                        for f in _featurise_negated(e_ann_id, ann_id_to_ann))
+                    ))
+                spec_f_tup = _featurise_speculated(e_ann_id, ann_id_to_ann)
+                argp.speculation_features.write('{}\t{}\n'.format(
+                    1 if e_ann_id in speculated else 0,
+                    ' '.join('{}:1'.format(f)
+                        for f in _featurise_speculated(e_ann_id, ann_id_to_ann))
+                    ))
+
+            if argp.verbose:
+                print >> stderr, 'Read {} annotations'.format(ann_i)
+
+    if argp.verbose:
+        print >> stderr, ('Saw a total of {} negated and {} speculated '
+                'annotations').format(seen_negated, seen_speculated)
+
+if __name__ == '__main__':
+    from sys import argv
+    exit(main(argv))
---
+++ src/lib/corpora.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+
+'''
+Functionality for interacting with BioNLP ST corpora structures.
+
+Author:     Pontus Stenetorp    <pontus stenetorp se>
+Version:    2012-03-15
+'''
+
+from os import walk
+from os.path import basename, exists, join as path_join, splitext
+
+from ann import parse_ann
+
+def _id(txt_path):
+    return splitext(basename(txt_path))[0]
+
+def _read_files(*fpaths):
+    for fpath in fpaths:
+        with open(fpath, 'r') as f:
+            for l in f:
+                yield l
+
+def read_corpora_dir(dir_path):
+    # The only assumption we can make is that any annotation must have a .txt
+    for root, dnames, fnames in walk(dir_path):
+        for txt_path in (path_join(root, f) for f in fnames
+                if f.endswith('.txt')):
+            doc_id = _id(txt_path)
+            ann_files = [f for f in (path_join(root, doc_id + ext)
+                for ext in ('.a1', '.a2', )) if exists(f)]
+            if not ann_files:
+                # Ignore the .txt since it lacks annotations
+                continue
+            yield doc_id, parse_ann(l.rstrip('\n')
+                    for l in _read_files(*ann_files))
